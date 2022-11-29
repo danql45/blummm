4b5d8ce9395ada6b88911da02d6167389ad50d3c
danql45 <danql1996@gmail.com>
2022-11-29 01:04:04 +0100

    simple json parse

---
+++ app/src/main/java/com/example/blum/Adapter.java
@@ -0,0 +1,72 @@
+package com.example.blum;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    List<String> titles;
+    List<Integer> images;
+    LayoutInflater inflater;
+    //smallacademy
+    public Adapter(Context ctx, List<String> titles, List<Integer> images){
+        this.titles = titles;
+        this.images = images;
+        this.inflater = LayoutInflater.from(ctx);
+    }
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.custom_grid_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(titles.get(position));
+        holder.gridIcon.setImageResource(images.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        ImageView gridIcon;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.textView);
+            gridIcon = itemView.findViewById(R.id.imageView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                Intent intent;
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(),"clicked ->" + getAdapterPosition(), Toast.LENGTH_SHORT).show();
+                    if (getAdapterPosition() == 1){
+                        intent = new Intent(view.getContext(), ProwadnikiActivity.class);
+                        view.getContext().startActivity(intent);
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/blum/MainActivity.java
+++ app/src/main/java/com/example/blum/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.blum;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView dataList;
+    List<String> titles;
+    List<Integer> images;
+
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dataList = findViewById(R.id.dataList);
+
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
+
+        titles.add("Zawiasy");
+        titles.add("Prowadniki");
+        titles.add("Third Item");
+        titles.add("Fourth Item");
+
+        images.add(R.drawable.ic_baseline_balance_24);
+        images.add(R.drawable.ic_baseline_beach_access_24);
+        images.add(R.drawable.ic_baseline_bluetooth_24);
+        images.add(R.drawable.ic_baseline_headphones_24);
+
+        adapter = new Adapter(this, titles, images);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
     }
 }
